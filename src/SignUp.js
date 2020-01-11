6e795d72c4f94a6e2ab7cacabe508e93f450d1b1
Ryan Talbert <ryantalbert@Ryans-Macintosh.local>
2020-01-11 12:27:45 -0600

    added fetch api for auth and sign in

---
+++ src/SignUp.js
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+import styles from "./styles/FormStyles";
+import { ThemeContext } from "./contexts/ThemeContext";
+
+export default function SignUp(props) {
+    const { isDarkMode } = useContext(ThemeContext);
+    const [isSignUp, setSignUp] = useState(false);
+    const { classes } = props;
+
+    const handleClick = () => {
+        setSignUp(true);
+      };
+    return (
+        
+    )
+}
